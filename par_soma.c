c5219dc9e05f67767b9c5c9fc114cc67717b1872
Ana Carolina <acarolbrianez@hotmail.com>
2025-04-02 10:34:47 -0300

    Add files via upload

---
+++ par_soma.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int somanum = 0;
+
+    printf("Digite um número de 1 a 6 (valor do dado jogado):\n");
+
+    while (1) {
+        printf("Digite um número: ");
+        scanf("%d", &num);
+        
+        if (num < 1) {
+            printf("Número inválido! Por favor, digite um número de 1 a 6.\n");
+        } else if (num > 6) {
+            printf("Número inválido! Por favor, digite um número de 1 a 6.\n");
+        } else if (num % 2 == 1) {
+            break;
+        } else {
+            somanum += num;
+        }
+    }
+
+    printf("O jogo terminou. A soma total dos números pares digitados é: %d\n", somanum);
+
+    return 0;
+}
