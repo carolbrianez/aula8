aaf982b2196f0d4e78a1e5007c9b5c907782276a
Ana Carolina <acarolbrianez@hotmail.com>
2025-04-08 21:20:01 -0300

    tabverdadeOR.c

--- tabverdadeOR.c
+++ tabverdadeOR.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int A, B;
+    int A;
+    int B;
 
     printf("Tabela Verdade\n");
     printf("----------------------------\n");
 
-    for (A = 0; A <= 1; A++) {
-        for (B = 0; B <= 1; B++) {
+    for (int A = 0; A <= 1; A++) {
+        for (int B = 0; B <= 1; B++) {
             printf("A: %d B: %d   %d || %d = %d\n", A, B, A, B, A || B);
         }
     }
